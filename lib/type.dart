26dca271346a2e4280175380c600a2d7a25cda19
isso0424 <isso04240129@gmail.com>
2020-08-24 01:44:05 +0900

    [Add]Create toJson and fromJson for SleepInfo

--- lib/type.dart
+++ lib/type.dart
@@ -6,7 +6,7 @@ class SleepInfo {
   SleepInfo(DateTime startSleeping, DateTime finishSleeping) {
     this.startSleeping = startSleeping;
     this.finishSleeping = this.finishSleeping.isBefore(startSleeping) ?
-      finishSleeping.add(new Duration(days: 1)) : finishSleeping;
+    finishSleeping.add(new Duration(days: 1)) : finishSleeping;
     this.date = DateTime(
         this.startSleeping.year,
         this.startSleeping.month,
@@ -17,4 +17,15 @@ class SleepInfo {
   Duration getTimeToSleep() {
     return this.finishSleeping.difference(this.startSleeping);
   }
+
+  SleepInfo.fromJson(Map<String, dynamic> json)
+      : startSleeping = DateTime.fromMillisecondsSinceEpoch(json["start"]),
+        finishSleeping = DateTime.fromMillisecondsSinceEpoch(json["finish"]),
+        date = DateTime.fromMillisecondsSinceEpoch(json["date"]);
+
+  Map<String, dynamic> toJson() => {
+    "start": startSleeping.millisecondsSinceEpoch,
+    "finish": finishSleeping.millisecondsSinceEpoch,
+    "date": finishSleeping.millisecondsSinceEpoch
+  };
 }
