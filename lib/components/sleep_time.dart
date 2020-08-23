1934c9d237deed13f2b27089c1f1667be20ce2b7
isso0424 <isso04240129@gmail.com>
2020-08-24 01:22:17 +0900

    [Add]Create sleepCard child widgets

---
+++ lib/components/sleep_time.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SleepTime extends StatelessWidget {
+  final Duration timeToSleep;
+  SleepTime({@required this.timeToSleep});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "${timeToSleep.inHours}h ${timeToSleep.inMinutes}min"
+    );
+  }
+}
+
+class SleepDuration extends StatelessWidget {
+  final DateTime startTime;
+  final DateTime finishTime;
+  SleepDuration({this.startTime, this.finishTime});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+        "${startTime.hour}:${startTime.minute} ~ ${finishTime.hour}:${finishTime.minute}"
+    );
+  }
+}
--- lib/type.dart
+++ lib/type.dart
@@ -1,11 +1,17 @@
 class SleepInfo {
   DateTime startSleeping;
   DateTime finishSleeping;
+  DateTime date;
 
   SleepInfo(DateTime startSleeping, DateTime finishSleeping) {
     this.startSleeping = startSleeping;
     this.finishSleeping = this.finishSleeping.isBefore(startSleeping) ?
       finishSleeping.add(new Duration(days: 1)) : finishSleeping;
+    this.date = DateTime(
+        this.startSleeping.year,
+        this.startSleeping.month,
+        this.startSleeping.day
+    );
   }
 
   Duration getTimeToSleep() {
