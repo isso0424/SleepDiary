662fd8ee4e12b94bf98adc32ddb13d76b05a8453
isso0424 <isso04240129@gmail.com>
2020-08-24 02:08:19 +0900

    [Add]Create listBuilder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sleep_diary/components/sleep_card.dart';
+import 'package:sleep_diary/data/data.dart';
+import 'package:sleep_diary/type.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -8,6 +11,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Data data;
+  _HomePageState() {
+    this.data = Data();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +24,22 @@ class _HomePageState extends State<HomePage> {
           widget.title
         ),
       ),
-      body: Text("kasu"),
+      body: FutureBuilder(
+        future: data.loadAllDates(),
+        builder: (BuildContext context, AsyncSnapshot<List<SleepInfo>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                SleepInfo sleepInfo = snapshot.data[index];
+                return SleepCard(sleepInfo: sleepInfo);
+              },
+            );
+          }
+
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
